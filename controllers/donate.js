afabc1315ed8b0cff101198cc790eb8922b6ec92
Jessica Orozco <jessicaorozco@gmail.com>
2024-04-15 00:47:40 +0200

    update paypal donation

---
+++ controllers/donate.js
@@ -0,0 +1,12 @@
+app.controller("DonateController", function ($scope, $location, qrService) {
+    $scope.donation = {
+        name: "Jess",
+        email: "jess@gmail.com",
+        amount: 2
+        // name: $scope.donation.name,
+        // email: $scope.donation.email,
+        // amount: $scope.donation.amount
+};
+
+
+});
--- controllers/qr.js
+++ controllers/qr.js
@@ -1,6 +1,7 @@
 app.controller("QrController", function ($scope, $location, qrService) {
   var qrcode = new QRCode("qrcode");
   var elText = document.getElementById("text");
+  $scope.inputValue = '';
 
     function makeCode() {
     qrcode.makeCode(elText.value);
@@ -17,6 +18,21 @@ app.controller("QrController", function ($scope, $location, qrService) {
         makeCode();
       }
     });
+
+    PayPal.Donation.Button({
+      env: 'production',
+      business: 'jessicaorozco@gmail.com',
+      
+      image: {
+          src: 'https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif',
+          title: 'PayPal - The safer, easier way to pay online!',
+          alt: 'Donate with PayPal button'
+      },
+      onComplete: function (params) {
+        $location.path("/donate");
+      },
+  }).render('#paypal-button-container');
+
     // $scope.generatePdf = function () {
     //   var dataURL = qrcode.toDataURL('image/png');
     //   var doc = new jsPDF();
@@ -27,6 +43,11 @@ app.controller("QrController", function ($scope, $location, qrService) {
     //   const fileURL = URL.createObjectURL(blob);
     //    window.open(fileURL);
     // };
+    $scope.clearInput = function() {
+      $scope.inputValue = ''; // Clear the input value
+      $('.clear-icon').hide(); // Hide the "X" icon
+    };
+
   $scope.resetForm = function () {
     resetObjectProperties($scope.qr, ["url"]);
   };
@@ -36,9 +57,14 @@ app.controller("QrController", function ($scope, $location, qrService) {
   }
 
   $scope.returnToList = function () {
-    $location.path("/task");
+    $location.path("/chart");
   };
 
+  $scope.pay = function () {
+    $location.path("https://www.paypal.com/donate/?hosted_button_id=LFDKPAPWX4NW2");
+  };
+  
   console.log(localStorage.getItem("qrs"));
   console.log($scope.qrs);
 });
+
